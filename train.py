8d7de18f900039bde7653b93269be35ba24adf0c
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-05-09 10:05:51 -0400

    initial import of tutorial code

---
+++ train.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+"""
+Code taken from:
+https://colab.research.google.com/github/omarsar/pytorch_notebooks/blob/master/pytorch_quick_start.ipynb#scrollTo=BZz7LAewgGAK
+"""
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+BATCH_SIZE = 32
+
+## transformations
+transform = transforms.Compose(
+    [transforms.ToTensor()])
+
+## download and load training dataset
+trainset = torchvision.datasets.MNIST(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
+                                          shuffle=True, num_workers=2)
+
+## download and load testing dataset
+testset = torchvision.datasets.MNIST(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
+                                         shuffle=False, num_workers=2)
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+
+        # 28x28x1 => 26x26x32
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
+        self.d1 = nn.Linear(26 * 26 * 32, 128)
+        self.d2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        # 32x1x28x28 => 32x32x26x26
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        # flatten => 32 x (32*26*26)
+        x = x.flatten(start_dim = 1)
+
+        # 32 x (32*26*26) => 32x128
+        x = self.d1(x)
+        x = F.relu(x)
+
+        # logits => 32x10
+        logits = self.d2(x)
+        out = F.softmax(logits, dim=1)
+        return out
+
+## test the model with 1 batch
+model = MyModel()
+for images, labels in trainloader:
+    print("batch size:", images.shape)
+    out = model(images)
+    print(out.shape)
+    break
+
+learning_rate = 0.001
+num_epochs = 5
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = MyModel()
+model = model.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+## compute accuracy
+def get_accuracy(logit, target, batch_size):
+    ''' Obtain accuracy for training round '''
+    corrects = (torch.max(logit, 1)[1].view(target.size()).data == target.data).sum()
+    accuracy = 100.0 * corrects/batch_size
+    return accuracy.item()
+
+for epoch in range(num_epochs):
+    train_running_loss = 0.0
+    train_acc = 0.0
+
+    model = model.train()
+
+    ## training step
+    for i, (images, labels) in enumerate(trainloader):
+        
+        images = images.to(device)
+        labels = labels.to(device)
+
+        ## forward + backprop + loss
+        logits = model(images)
+        loss = criterion(logits, labels)
+        optimizer.zero_grad()
+        loss.backward()
+
+        ## update model params
+        optimizer.step()
+
+        train_running_loss += loss.detach().item()
+        train_acc += get_accuracy(logits, labels, BATCH_SIZE)
+    
+    model.eval()
+    print('Epoch: %d | Loss: %.4f | Train Accuracy: %.2f' \
+          %(epoch, train_running_loss / i, train_acc/i))  
+
+test_acc = 0.0
+for i, (images, labels) in enumerate(testloader, 0):
+    images = images.to(device)
+    labels = labels.to(device)
+    outputs = model(images)
+    test_acc += get_accuracy(outputs, labels, BATCH_SIZE)
+        
+print('Test Accuracy: %.2f'%( test_acc/i))
